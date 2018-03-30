090a1db14b563cd957a76f504b45dfa3bbb819e1
Leon <leonduantian@hotmail.com>
2018-03-30 18:11:09 -0500

    fixed timer.h

--- Lab8/project/Lab8/Timer.h
+++ Lab8/project/Lab8/Timer.h
@@ -1,6 +1,4 @@
 void Timer0A_Init(void(*task)(void), uint32_t period);
-void Timer0B_Init(void(*task)(void), uint32_t period);
 void Timer1A_Init(void(*task)(void), uint32_t period);
-void Timer1B_Init(void(*task)(void), uint32_t period);
 void Timer2A_Init(void(*task)(void), uint32_t period);
-void Timer2B_Init(void(*task)(void), uint32_t period);
+void Timer3A_Init(void(*task)(void), uint32_t period);
