a268c2272ef2e64835f2b6569ec5ad5fcc7cf322
Tahir Haideri <tahirhaideri@gmail.com>
2018-04-01 16:49:43 -0500

    switch.c done, changes to timer.c and timer.h

--- Lab8/project/Lab8/Timer.c
+++ Lab8/project/Lab8/Timer.c
@@ -57,12 +57,11 @@ void Timer1A_Handler(void){
   (*PeriodicTask1A)();                // execute user task
 }
 
-void Timer2A_Init(void(*task)(void), uint32_t period){
-  SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
+void Timer2A_Arm(void(*task)(void), uint32_t period){
   PeriodicTask2A = task;          // user function
   TIMER2_CTL_R = 0x00000000;    // 1) disable timer2A during setup
   TIMER2_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
-  TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER2_TAMR_R = 0x00000001;   // 3) configure for one-shot mode settings
   TIMER2_TAILR_R = period-1;    // 4) reload value
   TIMER2_TAPR_R = 0;            // 5) bus clock resolution
   TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
--- Lab8/project/Lab8/Timer.h
+++ Lab8/project/Lab8/Timer.h
@@ -1,4 +1,4 @@
 void Timer0A_Init(void(*task)(void), uint32_t period);
 void Timer1A_Init(void(*task)(void), uint32_t period);
-void Timer2A_Init(void(*task)(void), uint32_t period);
+void Timer2A_Arm(void(*task)(void), uint32_t period);
 void Timer3A_Init(void(*task)(void), uint32_t period);
