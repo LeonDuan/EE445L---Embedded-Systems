f458d5cdecbd613d743fdbc1eee55e1ba84678d5
Tahir Haideri <tahirhaideri@gmail.com>
2018-04-12 12:55:09 -0500

    prep for lab10

---
+++ Lab10/prelab/Tach.c
@@ -0,0 +1,60 @@
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+#include "PLL.h"
+
+#define NVIC_EN0_INT19          0x00080000  // Interrupt 19 enable
+#define PF2                     (*((volatile uint32_t *)0x40025010))
+#define TIMER_TAMR_TACMR        0x00000004  // GPTM TimerA Capture Mode
+#define TIMER_TAMR_TAMR_CAP     0x00000003  // Capture mode
+#define TIMER_CTL_TAEN          0x00000001  // GPTM TimerA Enable
+#define TIMER_CTL_TAEVENT_POS   0x00000000  // Positive edge
+#define TIMER_CTL_TAEVENT_NEG   0x00000004  // Negative edge
+#define TIMER_CTL_TAEVENT_BOTH  0x0000000C  // Both edges
+#define TIMER_IMR_CAEIM         0x00000004  // GPTM CaptureA Event Interrupt Mask
+#define TIMER_ICR_CAECINT       0x00000004  // GPTM CaptureA Event Interrupt Clear
+#define TIMER_TAILR_TAILRL_M    0x0000FFFF  // GPTM TimerA Interval Load Register Low
+
+uint32_t Period;              // (1/clock) units
+uint32_t First;               // Timer0A first edge
+int32_t Done;                 // set each rising
+
+void Tach_Init(void){
+  SYSCTL_RCGCTIMER_R |= 0x01;// activate timer0    
+  SYSCTL_RCGCGPIO_R |= 0x22;       // activate port B and port F
+                                   // allow time to finish activating
+  First = 0;                       // first will be wrong
+  Done = 0;                        // set on subsequent
+  GPIO_PORTB_DIR_R &= ~0x40;       // make PB6 in
+  GPIO_PORTB_AFSEL_R |= 0x40;      // enable alt funct on PB6/T0CCP0
+  GPIO_PORTB_DEN_R |= 0x40;        // enable digital I/O on PB6
+                                   // configure PB6 as T0CCP0
+  GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R&0xF0FFFFFF)+0x07000000;
+  GPIO_PORTB_AMSEL_R &= ~0x40;     // disable analog functionality on PB6
+  TIMER0_CTL_R &= ~TIMER_CTL_TAEN; // disable timer0A during setup
+  TIMER0_CFG_R = TIMER_CFG_16_BIT; // configure for 16-bit timer mode
+                                   // configure for 24-bit capture mode
+  TIMER0_TAMR_R = (TIMER_TAMR_TACMR|TIMER_TAMR_TAMR_CAP);
+                                   // configure for rising edge event
+  TIMER0_CTL_R &= ~(TIMER_CTL_TAEVENT_POS|0xC);
+  TIMER0_TAILR_R = TIMER_TAILR_TAILRL_M;// start value
+  TIMER0_TAPR_R = 0xFF;            // activate prescale, creating 24-bit
+  TIMER0_IMR_R |= TIMER_IMR_CAEIM; // enable capture match interrupt
+  TIMER0_ICR_R = TIMER_ICR_CAECINT;// clear timer0A capture match flag
+  TIMER0_CTL_R |= TIMER_CTL_TAEN;  // enable timer0A 16-b, +edge timing, interrupts
+                                   // Timer0A=priority 2
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
+  NVIC_EN0_R = NVIC_EN0_INT19;     // enable interrupt 19 in NVIC
+}
+void Timer0A_Handler(void){
+  PF2 = PF2^0x04;  // toggle PF2
+  PF2 = PF2^0x04;  // toggle PF2
+  TIMER0_ICR_R = TIMER_ICR_CAECINT;// acknowledge timer0A capture match
+  Period = (First - TIMER0_TAR_R)&0xFFFFFF;// 24 bits, 12.5ns resolution
+  First = TIMER0_TAR_R;            // setup for next
+  Done = 1;
+  PF2 = PF2^0x04;  // toggle PF2
+}
+
+uint32_t Tach_Read(){
+	return Period;
+}
---
+++ Lab10/prelab/Tach.h
@@ -0,0 +1,2 @@
+void Tach_Init(void);
+uint32_t Tach_Read(void);
